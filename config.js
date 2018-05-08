d43390e6fce30e9c95f61563ae8793f61dbd0794
Edward McCarthy <efmccarthy26@gmail.com>
2018-05-07 18:58:31 -0600

    Linked server and config.js. Throwing the unhandled promise rejection, though it still works. Will attend to that and add catch later, after first endpoint testing

---
+++ config.js
@@ -0,0 +1,8 @@
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+                       global.DATABASE_URL ||
+                       "mongodb://localhost/personal-library";
+
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
+                          'mongodb://localhost/test-personal-library';
+
+exports.PORT = process.env.PORT || 8080;
---
+++ server.js
@@ -0,0 +1,53 @@
+"use strict"
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const {PORT, DATABASE_URL} = require("./config");
+mongoose.Promise = global.Promise;
+
+
+const app = express();
+app.use(bodyParser.json());
+
+function runServer(databaseUrl, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on ${port}`)
+        resolve();
+      })
+
+      .on("error", err => {
+        mongoose.disconnect();
+        reject(err)
+      })
+    })
+  })
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
