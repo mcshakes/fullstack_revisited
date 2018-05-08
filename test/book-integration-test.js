7607e3d6cc5a3980e857eb6996a0a328e2f6b850
Edward McCarthy <efmccarthy26@gmail.com>
2018-05-07 21:38:53 -0600

    Starting with POST since a user would add the books to the library first

--- test/book-integration-test.js
+++ test/book-integration-test.js
@@ -1,3 +1,4 @@
+const chai = require("chai");
 const chaiHttp = require("chai-http");
 const faker = require("faker");
 const mongoose = require("mongoose");
@@ -29,3 +30,43 @@ function generateFakeBook() {
     }
   }
 }
+
+function teardownDB() {
+  console.warn("Deleting the Database");
+  return mongoose.connection.dropDatabase();
+}
+
+describe("Books API Resource", function() {
+
+  describe("POST endpoint", function() {
+
+    it("should add a new book", function() {
+      const newBook = {
+        title: faker.lorem.words(),
+        author: {
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName()
+        }
+      }
+
+      return chai.request(app)
+        .post("/books")
+        .send(newBook)
+        .then(function(res) {
+          expect(res).to.have.status(201);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a("object");
+          expect(res.body).to.include.keys(
+            "id", "title", "author"
+          );
+          expect(res.body.author).to.equal(newBook.author)
+          expect(res.body.id).to.not.be.null;
+          return Book.findById(res.body.id);
+        })
+        .then(function(book){
+          expect(book.title).to.equal(newBook.title);
+          // can add more
+        })
+    })
+  }); // End of POST endpoint
+})
