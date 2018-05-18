ab28895fa60f38573272cade470b7bee04319c14
Edward McCarthy <efmccarthy26@gmail.com>
2018-05-18 15:06:37 -0600

    Added my own API helper"

---
+++ client/src/helpers/BackEndAPI.js
@@ -0,0 +1,32 @@
+const URL = "http://localhost:3000";
+
+class BackEndAPI {}
+
+BackEndAPI.fetch = (endpoint, method, data) => {
+  let options = {
+    method: method,
+    headers: new Headers({
+      "Content-Type": "application/json"
+    })
+  }
+
+  return fetch(URL + endpoint, options).then(
+    response => {
+      if (response.status === 204 && method === "DELETE") {
+        return { success: true };
+      } else {
+        return response.json();
+      }
+    },
+    error => {
+      // network level errors
+      console.log(error);
+    }
+  )
+};
+
+BackEndAPI.getBooks = () => {
+  return BackEndAPI.fetch("/books", "GET")
+}
+
+export default BackEndAPI;
