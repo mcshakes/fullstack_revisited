80b185f216f32b7f820b153830fb5e000598fb42
Edward McCarthy <efmccarthy26@gmail.com>
2018-05-18 15:13:26 -0600

    Calling the API, and setting state of the Books to be whatever is coming in from the response

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import BackEndAPI from "./helpers/BackEndAPI";
 
 class App extends Component {
-  state = {
-    response: ''
-  };
-
-  componentDidMount() {
-    this.callApi()
-      .then(
-        // console.log(res)
-        )
-      .catch(err => console.log(err));
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    }
   }
 
-  callApi = async () => {
-    const response = await fetch('http://localhost:8080/books');
-    debugger
-    const body = await response.json();
+  getBooks() {
+    BackEndAPI.getBooks()
+      .then(data => {
+        this.setState({
+          books: data.books
+        })
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState({
+          books: []
+        })
+      })
+  }
 
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
+  componentDidMount() {
+    this.getBooks();
+  }
 
   render() {
     console.log(this)
