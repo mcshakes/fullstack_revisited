acbac4d85b59fb7ca496b726d2c7c92677209560
Edward McCarthy <efmccarthy26@gmail.com>
2018-05-18 11:37:40 -0600

    Finally, seeing the response within the network. Need to make a proper fetch API call, since it's not rendering in the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(
+        // console.log(res)
+        )
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('http://localhost:8080/books');
+    debugger
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
   render() {
-    console.log("POOP")
+    console.log(this)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">{this.state.response}</p>
       </div>
     );
   }
