4617671e37bd25d18d9b0f021d8aa632fe46f9c2
Edward McCarthy <efmccarthy26@gmail.com>
2018-06-13 18:23:33 -0600

    Conditionals are working now and the POST request to save a book is good as well

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   $(".delete-book").on("click", function(e) {
     $target = $(e.target);
     const id = $target.attr("data-id");
@@ -14,4 +15,36 @@ $(document).ready(function() {
       }
     })
   });
+
+
+  $(".add-to-library").on("click", function(e) {
+
+    $target = $(e.target)
+    const title = $target.attr("data-title")
+    const desc = $target.attr("data-desc")
+    const author = $target.attr("data-author")
+
+    let image;
+    if ($target.attr("data-cover")) {
+      image = $target.attr("data-cover")
+    }
+
+    $.ajax({
+      type: "POST",
+      url: "/books",
+      data: {
+        title: title,
+        summary: desc,
+        author: author,
+        image: (image ? image : null)
+      },
+      success: function(res) {
+        window.location.href = "/books"
+        // alert(image)
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    })
+  })
 })
