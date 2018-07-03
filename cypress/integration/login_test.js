d56c0810c4a62d4d46ec1da59dc7714bcc92d2cd
Edward McCarthy <efmccarthy26@gmail.com>
2018-07-03 13:16:29 -0600

    tests are working as a whole; separating them out for logged in users

--- cypress/integration/login_test.js
+++ cypress/integration/login_test.js
@@ -1,48 +1,65 @@
 describe("Authenticated user visits the site", function() {
 
-  before(function() {
-    cy.visit("/books")
-    cy.contains("Log In").click()
+  context("succesful login", function() {
+
+    before(function() {
+      cy.visit("/books")
+      cy.contains("Log In").click()
+    })
+
+    it("greets user with a form of rlogin", function() {
+      cy.url().should("include", "/login")
+      cy.contains("h1", "Login")
+    })
+
+    it("redirects to book library on success", function() {
+      cy.get('input[type=email]')
+        .type('crap2@example.com')
+        .should("have.value", "crap2@example.com")
+
+      cy.get('input[type=password]')
+        .type('password1{enter}')
+
+      cy.url().should('include', '/users')
+
+      cy.getCookie('connect.sid').should('exist')
+      cy.get("nav ul").children().should("have.length", 2)
+    })
+
+    it("has books already existing for that user", function() {
+      cy.get(".books").contains("Flower Power!")
+    })
+
+    it("can request to search books", function() {
+      cy.contains("Add a New Book").click()
+      cy.url().should('include', '/search')
+      cy.get("form").should("be.visible")
+      cy.get("form").should("have.value", "")
+    })
+
+    // it("can log out", function() {
+    //   cy.get("nav").contains("Log Out").click()
+    //   cy.url().should('include', '/book')
+    // })
+
   })
 
-  it("clicks login and sees the form", function() {
-    cy.url().should("include", "/login")
-  })
+  // xit("displays errors on login", function() {
+  //   cy.get('input[name=username]').type('jane.lae')
+  //         cy.get('input[name=password]').type('password123{enter}')
+  //
+  //         // we should have visible errors now
+  //         cy.get('p.error')
+  //           .should('be.visible')
+  //           .and('contain', 'Username and/or password is incorrect')
+  //
+  //         // and still be on the same URL
+  //   cy.url().should('include', '/login')
+  // })
 
-  xit("displays errors on login", function() {
-    cy.get('input[name=username]').type('jane.lae')
-          cy.get('input[name=password]').type('password123{enter}')
-
-          // we should have visible errors now
-          cy.get('p.error')
-            .should('be.visible')
-            .and('contain', 'Username and/or password is incorrect')
-
-          // and still be on the same URL
-    cy.url().should('include', '/login')
-  })
-
-  it("redirects to book library on success", function() {
-    // cy.get('#username-email')
-    cy.get('input[type=email]')
-      .type('crap2@example.com')
-      .should("have.value", "crap2@example.com")
-
-    cy.get('input[type=password]')
-      .type('password1{enter}')
-    // we should be redirected to /dashboard
-    cy.url().should('include', '/users')
-    // cy.get('h1').should('contain', 'jane.lane')
-
-    cy.getCookie('connect.sid').should('exist')
-  })
-
-  it("can request other authenticated pages", function() {
-    // cy.visit("/search")
-    cy.contains("Add a New Book").click()
-      .its("form")
-      .should("include", 'input[name=title]')
-  })
-
-
+  // it greets with sign in
+  // it requires email
+  // it requires password
+  // it requires valid username and password
+  // it navigates to #/ on succesful login
 })
