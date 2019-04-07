2b7f6494664dc7e72b4fa162ddc1ce6539ba8909
mcshakes <efmccarthy26@gmail.com>
2019-04-07 16:36:13 -0600

    Removed debuggers

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,6 +7,7 @@ const { User } = require("../models/user")
 
 const isLoggedIn = (req, res, next) => {
   if(req.isAuthenticated()) {
+
     // console.log(req.sessionID)
     // console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
     // console.log(`req.user: ${JSON.stringify(req.user)}`)
